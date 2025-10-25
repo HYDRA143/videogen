8d767dbf94ddc148e01c5ed745aaa0dcfc882dac
NICK <146691602+HYDRA143@users.noreply.github.com>
2025-10-25 16:04:13 +0000

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# videogen
-Test
---
+++ portfolio/src/components/content.tsx
@@ -0,0 +1,22 @@
+
+
+const content = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default content
